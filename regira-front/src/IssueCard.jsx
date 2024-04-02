a43e1b52c45433c9a46a7f88b7d3f9d1c8c555d7
MartaGranja <mgn9907@gmail.com>
2024-04-02 19:43:19 +0200

    arreglo diseño kanban

--- regira-front/src/IssueCard.jsx
+++ regira-front/src/IssueCard.jsx
@@ -2,14 +2,14 @@ const State = ({value}) => <h2>{value}</h2>
 const Priority = ({value}) => <h2>{value}</h2> 
 const Type = ({value}) => <h2>{value}</h2> 
 
-const getColorByType  = ({type}) => {
+const getColorByType  = ({priority}) => {
     
-    if (type==='bug'){
-        return 'bg-red-400';
-    } else if (type==='feature'){
-        return 'bg-green-400';
-    } else { // task
-        return 'bg-blue-400'
+    if (priority==='high'){
+        return 'bg-red-200';
+    } else if (priority==='low'){
+        return 'bg-green-200';
+    } else { // mid
+        return 'bg-orange-200'
     }
 }
 
@@ -18,12 +18,19 @@ export default ({data, reference, isDragging, remove}) =>  {
 
     return (
         <>
-            <div ref = {reference}  className={"border p-2 m-3 "+getColorByType(data)}>
-                <h1>{data.title} {data.id}</h1>
-                <Type value={data.type} />
+            <div ref = {reference}  className={"border p-3 m-3 rounded-lg "+getColorByType(data)}>
+                <h1 className="font-bold">{data.name}</h1>
+                <p>{data.description}</p>
+                <br /><br />
+                <h2 className="bg-slate-500 text-white rounded-lg mb-2 p-2 w-1/2">{data.state}</h2>
+                <h2 className="bg-slate-500 text-white rounded-lg mb-2 p-2 w-1/2">{data.task_type}</h2>
+                <h2 className="bg-slate-500 text-white rounded-lg mb-2 p-2 w-1/2">{data.priority}</h2>
+
+
+                {/*<Type value={data.type} />
                 <Priority value={data.priority} />
-                <State value={data.state} />
-                <button className="border p-2 bg-red-600 text-white" onClick={()=>remove(data)}>Elimina</button>
+                <State value={data.state} />*/}
+                <button className="p-2 bg-red-600 hover:bg-red-400 mt-2 rounded-lg text-white" onClick={()=>remove(data)}>Elimina</button>
             </div>
         </>
     )
--- regira-front/src/NovaTask.jsx
+++ regira-front/src/NovaTask.jsx
@@ -1,118 +1,167 @@
 import { useState } from "react";
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from "react-router-dom";
 import Context from "./Context";
 import { useContext } from "react";
 
 function NovaTasca() {
+  const { projectId } = useParams();
 
-    const { projectId } = useParams();
+  const { logout, API_URL } = useContext(Context);
 
-    const {logout, API_URL} = useContext(Context);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState("");
+  const [state, setState] = useState("");
+  const [task_type, setTask_type] = useState("");
+  const redirect = useNavigate();
 
-    const [name, setName] = useState("");
-    const [description, setDescription] = useState("");
-    const [priority, setPriority] = useState("");
-    const [state, setState] = useState("");
-    const [task_type, setTask_type] = useState("");
-    const redirect = useNavigate();
-    
+  const creaTasca = (e) => {
+    e.preventDefault();
 
-    const creaTasca = (e) => {
+    // console.log("creant tasca..", name, description, active)
 
-        e.preventDefault();
+    const credencials = {
+      name,
+      description,
+      priority,
+      state,
+      task_type,
+    };
 
-        // console.log("creant tasca..", name, description, active)
+    const opcions = {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(credencials),
+    };
 
-        const credencials = {
-            name,
-            description,
-            priority,
-            state,
-            task_type
-        }
+    fetch(API_URL + "/task/project/" + projectId, opcions)
+      .then((resp) => resp.json())
+      .then((data) => {
+        data.error == "Unauthorized"
+          ? logout()
+          : redirect("/project/task/" + projectId);
+      })
+      .catch((err) => console.log(err));
+  };
 
-        const opcions = {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json'
-              },
-            body: JSON.stringify(credencials)
-        }
-
-        fetch(API_URL + '/task/project/' + projectId, opcions)
-        .then(resp => resp.json())
-        .then(data => {
-            (data.error == "Unauthorized") ? logout() : redirect("/project/task/" + projectId);
-        })
-        .catch(err => console.log(err))
-    }
-
-
-
-
-    
-    return (
+  return (
     <>
-        <div className="w-full max-w-xs w-82 mx-auto">
-            <form onSubmit={creaTasca} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-                        Nom
-                    </label>
-                    <input
-                        onInput={(e) => setName(e.target.value)}
-                        value={name}
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="name" id="name" type="text" />
-                </div>
-                <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
-                        Descripció
-                    </label>
-                    <input
-                        onInput={(e) => setDescription(e.target.value)}
-                        value={description} className="shadow appearance-none border
-                    rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="description" id="description" type="text" />
-                </div>
-                <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
-                        Prioridad
-                    </label>
-                    <input
-                        onInput={(e) => setPriority(e.target.value)}
-                        value={priority} className="shadow appearance-none border
-                    rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="priority" id="priority" type="text" />
-                </div>
-                <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
-                        Estado
-                    </label>
-                    <input
-                        onInput={(e) => setState(e.target.value)}
-                        value={state} className="shadow appearance-none border
-                    rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="state" id="state" type="text" />
-                </div>
-                <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
-                        Tipo de tasca
-                    </label>
-                    <input
-                        onInput={(e) => setTask_type(e.target.value)}
-                        value={task_type} className="shadow appearance-none border
-                    rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="task_type" id="task_type" type="text" />
-                </div>
-                <div className="flex items-center justify-between">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                        Crear tasca
-                    </button>
-                </div>
-            </form>
-            
-        </div>
+      <div className="w-full max-w-xs w-82 mx-auto">
+        <form
+          onSubmit={creaTasca}
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="name"
+            >
+              Nom
+            </label>
+            <input
+              onInput={(e) => setName(e.target.value)}
+              value={name}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              name="name"
+              id="name"
+              type="text"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="description"
+            >
+              Descripció
+            </label>
+            <input
+              onInput={(e) => setDescription(e.target.value)}
+              value={description}
+              className="shadow appearance-none border
+                    rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              name="description"
+              id="description"
+              type="text"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="priority"
+            >
+              Prioridad
+            </label>
+            <select
+              onChange={(e) => setPriority(e.target.value)}
+              value={priority}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              name="priority"
+              id="priority"
+            >
+              <option value="high">High</option>
+              <option value="medium">Medium</option>
+              <option value="low">Low</option>
+            </select>
+          </div>
 
-        
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="description"
+            >
+              Estado
+            </label>
+            <select
+              onChange={(e) => setState(e.target.value)}
+              value={state}
+              className="shadow appearance-none border
+                    rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              name="state"
+              id="state"
+            >
+                <option value="backlog">Backlog</option>
+                <option value="in_progress">In progress</option>
+                <option value="review">Review</option>
+                <option value="done">Done</option>
+                <option value="closed">Closed</option>
+            </select>
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="description"
+            >
+              Tipo de tasca
+            </label>
+            <select
+              onChange={(e) => setTask_type(e.target.value)}
+              value={task_type}
+              className="shadow appearance-none border
+                    rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              name="task_type"
+              id="task_type"
+              type="text"
+            >
+                <option value="story">Story</option>
+                <option value="bugs">Bugs</option>
+                <option value="general">General</option>
+            </select>
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Crear tasca
+            </button>
+          </div>
+        </form>
+      </div>
     </>
-        
-)}
+  );
+}
 
 export default NovaTasca;
